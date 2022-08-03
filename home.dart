86eb5a1c74376faef1168e35045420a2766abe38
danapham0821 <107631182+danapham0821@users.noreply.github.com>
2022-08-02 20:26:41 -0400

    Add files via upload

---
+++ home.dart
@@ -0,0 +1,69 @@
+import 'package:bmi/constants/app_constants.dart';
+import 'package:bmi/widgets/left_bar.dart';
+import 'package:bmi/widgets/right_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          tittle: Text(
+            "BMI Calculator",
+            style:
+                TextStyle(color: accentHexColor, fontWeight: FontWeight.w300),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          centerTitle: true,
+        ),
+        backgroundColor: mainHexColor,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+                Container(
+                  width: 130,
+                  child: TextField(
+                    style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.w300,
+                        color: accentHexColor),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Height",
+                      hintStyle: TextStyle(
+                          fontSize: 42,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.white.withOpacity(.8)),
+                    ),
+                  ),
+                ),
+              ]),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                child: Text(
+                  "Calculate",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: accentHexColor,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
